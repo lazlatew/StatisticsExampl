1dd8868f98135961ee0452de212109bc99ccdbc9
Dimitar Zlatew <zlatewbusiness@gmail.com>
2025-09-25 14:52:36 +0300

    Add files via upload

---
+++ StatisticsExampl/Person.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StatisticsExampl
+{
+    internal class Person
+    {
+        //svoistva
+        public string Name { get; set; }
+        public int Age { get; set; }
+
+        // Konstruktor
+        public Person(string name, int age)
+        {
+            Name = name;
+            Age = age;
+        }
+    }
+}
---
+++ StatisticsExampl/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace StatisticsExampl
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine()); 
+
+            List<Person> people = new List<Person>(); //list za horata
+
+            for (int i = 0; i < n; i++)
+            {
+                string[] input = Console.ReadLine().Split(' ');
+                string name = input[0];
+                int age = int.Parse(input[1]);
+
+                Person person = new Person(name, age); // susdv na objct
+                people.Add(person); // advam v spisuka
+            }
+            
+            // 30 > godini & sort po A-red
+            List<Person> filtered = people
+                .Where(p => p.Age > 30)
+                .OrderBy(p => p.Name)
+                .ToList();
+
+            // Izhod
+
+            foreach (var person in filtered)
+            {
+                Console.WriteLine($"{person.Name} - {person.Age}");
+            }
+        }
+    }
+}
